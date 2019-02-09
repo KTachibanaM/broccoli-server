501f2f25f0dfb5901b3461b7186abb1f4530e3e8
KTachibanaM <whj19931115@gmail.com>
2019-02-09 14:24:24 +0800

    pause

--- broccoli/dev/add_example_workers.sh
+++ broccoli/dev/add_example_workers.sh
@@ -34,3 +34,19 @@ curl --request POST \
 	"global_args": ["image_s3"],
     "interval_seconds": 30
 }'
+
+curl --request POST \
+  --url http://localhost:5002/api/worker \
+  --header 'content-type: application/json' \
+  --data '{
+	"module": "workers.twitter_image_backfiller",
+	"class_name": "TwitterImageBackfiller",
+	"args": {
+	     "screen_name": "Wzettairyouiki",
+	     "init_tweet_id": 1094105684180897792,
+	     "round_count": 10,
+	     "pause_after_round": 1
+	},
+	"global_args": [],
+    "interval_seconds": 30
+}'
--- broccoli/workers/twitter_image_backfiller/__init__.py
+++ broccoli/workers/twitter_image_backfiller/__init__.py
@@ -5,15 +5,17 @@ from workers.twitter import get_media_urls, get_tweet_url
 
 
 MAX_ID_KEY = 'max_id'
+CURRENT_ROUND_KEY = "current_round"
+PAUSED_KEY = "paused"
 
 
 class TwitterImageBackfiller(BaseWorker):
-    def __init__(self, screen_name: str, init_tweet_id: int):
+    def __init__(self, screen_name: str, init_tweet_id: int, round_count: int, pause_after_round: int):
         super(TwitterImageBackfiller, self).__init__(f"twitter_image_backfiller.{screen_name}.{init_tweet_id}")
         self.screen_name = screen_name
         self.init_tweet_id = init_tweet_id
-        # todo: customize count
-        self.count = 10
+        self.round_count = round_count
+        self.pause_after_round = pause_after_round
         self.twitter_api = None
 
     def pre_work(self):
@@ -23,16 +25,30 @@ class TwitterImageBackfiller(BaseWorker):
             access_token_key=os.getenv("TWITTER_ACCESS_TOKEN_KEY"),
             access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
         )
+        self.metadata_store.set(MAX_ID_KEY, self.init_tweet_id)
+        self.metadata_store.set(CURRENT_ROUND_KEY, 0)
+        self.metadata_store.set(PAUSED_KEY, True)
 
     def work(self):
-        if not self.metadata_store.exists(MAX_ID_KEY):
-            self.metadata_store.set(MAX_ID_KEY, self.init_tweet_id)
         max_tweet_id = self.metadata_store.get(MAX_ID_KEY)
+        current_round = self.metadata_store.get(CURRENT_ROUND_KEY)
+        paused = self.metadata_store.get(PAUSED_KEY)
+        should_pause = self.pause_after_round > 0 and current_round == self.pause_after_round and paused
+        if should_pause:
+            self.logger.info(f"Pause work because pause_after_round={self.pause_after_round} > 0 "
+                             f"AND current_round={current_round} == pause_after_round={self.pause_after_round} "
+                             f"AND paused")
+            return
+        if current_round == self.pause_after_round and not paused:
+            self.logger.info(f"Resetting current_round to 0 and paused")
+            current_round = 0
+            self.metadata_store.set(CURRENT_ROUND_KEY, 0)
+            self.metadata_store.set(PAUSED_KEY, True)
 
         tweets = self.twitter_api.GetUserTimeline(
             screen_name=self.screen_name,
             max_id=max_tweet_id,
-            count=self.count
+            count=self.round_count
         )
         if not tweets:
             self.logger.warning(f"No tweet to process")
@@ -54,3 +70,5 @@ class TwitterImageBackfiller(BaseWorker):
             self.rpc_client.blocking_append(idempotency_key="image_url", doc=new_document)
 
         self.metadata_store.set(MAX_ID_KEY, min_tweet_id - 1)
+        if self.pause_after_round > 0 and current_round < self.pause_after_round:
+            self.metadata_store.set(CURRENT_ROUND_KEY, current_round + 1)
