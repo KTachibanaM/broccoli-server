d28c62e6f29ccb5599e28410224986d9ea5873ef
KTachibanaM <whj19931115@gmail.com>
2019-03-09 23:15:24 -0800

    this is the last one

--- broccoli-web/src/boardProjections/image.js
+++ broccoli-web/src/boardProjections/image.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function () {
   return function (props) {
     const {document} = props;
-    const imageUrl = `${process.env.REACT_APP_S3_HOSTNAME}:${process.env.REACT_APP_S3_PORT}/${process.env.REACT_APP_S3_BUCKET_NAME}/${document["s3_image_id"]}`;
+    const imageUrl = `http://${process.env.REACT_APP_S3_HOSTNAME}:${process.env.REACT_APP_S3_PORT}/${process.env.REACT_APP_S3_BUCKET_NAME}/${document["s3_image_id"]}`;
     return (
       <div style={{"width": "200px", "height": "120px"}}>
         <img src={imageUrl} alt={imageUrl} style={{"width": "auto", "height": "100%"}}/>
