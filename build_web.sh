04a21c5c2ed6bfdd78bb397a5a729bb983de22c7
KTachibanaM <whj19931115@gmail.com>
2019-03-12 00:06:48 -0700

    COMMENTS

--- build_web.sh
+++ build_web.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Checking and exporting required environment"
 [ -z "$PUBLIC_URL" ] && echo "Need to set \$PUBLIC_URL" && exit 1;
 [ -z "$REACT_APP_SERVER_URL" ] && echo "Need to set \$REACT_APP_SERVER_URL" && exit 1;
 export PUBLIC_URL=$PUBLIC_URL
 export REACT_APP_SERVER_URL=$REACT_APP_SERVER_URL
 
+echo "Building web"
 cd web
 npm run build
 cd ..
+
+echo "Moving ./web/build to ./server/web_static"
 rm -rf ./server/web_static
 cp -r ./web/build ./server/web_static
