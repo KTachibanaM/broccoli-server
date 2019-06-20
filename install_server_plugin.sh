dec5504be308f3ecfec211e7ffe41d61853c8612
KTachibanaM <whj19931115@gmail.com>
2019-06-19 18:34:21 -0700

    no more dockerfile for now

--- install_server_plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Checking required environment"
-[ -z "$SERVER_PLUGIN" ] && echo "Need to set \$SERVER_PLUGIN" && exit 1;
-
-echo "Installing $SERVER_PLUGIN"
-pip install pip==18.1
-pip install $SERVER_PLUGIN --process-dependency-links
