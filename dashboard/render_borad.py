8b8f017e314466990da38377e88f48ccc36bb48f
KTachibanaM <whj19931115@gmail.com>
2019-06-22 01:04:04 -0700

    skeleton

--- dashboard/render_borad.py
+++ dashboard/render_borad.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Union
+from broccoli_plugin_interface.board.render import Render, CallbackRender
 from .objects.board_query import BoardQuery
 
 
-def render_board(board_query: BoardQuery) -> List:
+def render_board(board_query: BoardQuery) -> List[Union[Render, CallbackRender]]:
     pass
