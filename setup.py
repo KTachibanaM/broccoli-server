156355e7a5509bd2a504d119aba36d061c9c602d
KTachibanaM <whj19931115@gmail.com>
2019-10-11 22:28:28 -0700

    remove dependency link yayy

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-install_requires=[
+install_requires = [
     'jinja2==2.10.1',
     'flask==1.0.3',
     'pika==1.0.1',
@@ -10,15 +10,11 @@ install_requires=[
     'dnspython==1.16.0',
     'jsonschema==3.0.1',
     'apscheduler==3.6.0',
-    'python-dotenv==0.10.3',
-    'pymongo-schema'
+    'pymongo-schema @ https://github.com/pajachiet/pymongo-schema/tarball/0dd312c23f5d38fb7c1a1f3bbe977e79a814a119'
+    '#egg=pymongo-schema'
 ]
 
-dependency_links=[
-    'https://github.com/pajachiet/pymongo-schema/tarball/0dd312c23f5d38fb7c1a1f3bbe977e79a814a119#egg=pymongo-schema'
-]
-
-tests_require=[
+tests_require = [
     'mongomock==3.17.0',
     'freezegun==0.3.12'
 ]
@@ -34,6 +30,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=install_requires,
-    dependency_links=dependency_links,
     tests_require=tests_require
 )
