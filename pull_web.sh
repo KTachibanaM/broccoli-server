f93e12a94304f0357615995763c560cdb0d90ab2
KTachibanaM <whj19931115@gmail.com>
2019-11-02 22:31:06 -0700

    put web into package itself

--- pull_web.sh
+++ pull_web.sh
@@ -4,6 +4,6 @@ set -e
 rm -f web.tar.gz
 curl -O -L https://github.com/broccoli-platform/broccoli-web/releases/download/"$(cat WEB_VERSION.txt)"/web.tar.gz
 
-rm -rf web
-mkdir web
-tar xzf web.tar.gz -C web
+rm -rf broccoli_server/web
+mkdir -p broccoli_server/web
+tar xzf web.tar.gz -C broccoli_server/web
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = [
 
 setup(
     name='broccoli_server',
-    version='0.1.3',
+    version='0.1.4',
     description='The server component of a web content crawling and sorting framework',
     url='http://github.com/KTachibanaM/broccoli-platform',
     author='KTachibanaM',
