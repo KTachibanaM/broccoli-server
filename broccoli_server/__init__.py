a76bb042c448c7297a5face06e4a0717944f628b
KTachibanaM <whj19931115@gmail.com>
2020-06-17 19:13:13 -0700

    fix logging level

--- broccoli_server/__init__.py
+++ broccoli_server/__init__.py
@@ -3,8 +3,9 @@ import os
 
 root_handler = logging.StreamHandler()
 root_handler.setFormatter(logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s] %(message)s"))
+logger = logging.getLogger(__name__)
+logger.addHandler(root_handler)
 if os.environ.get("LOGGING_DEBUG", "false") == "true":
-    root_handler.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
 else:
-    root_handler.setLevel(logging.INFO)
-logging.getLogger(__name__).addHandler(root_handler)
+    logger.setLevel(logging.INFO)
